2dd385be08266d7c7a8b7fc2df7e64b04597682c
Kai Fleischman <kaifleischmans@gmail.com>
2020-10-04 23:52:07 +0200

    should be done

--- src/testPackage/LogChecker.java
+++ src/testPackage/LogChecker.java
@@ -6,25 +6,74 @@ public class LogChecker {
 	* logs represent a correct sequential execution, and if it doesn't, return
 	* the number of logs that contain an operation with an invalid outcome.
 	*
-	* @param startSet	List representation of the set that was used in a test
+	* @param startList	LinkedList representation of the SkipList that was given
+	*					at the beginning of a multithreaded operation test
 	*
-	* @param opLogs		TreeMap of logs for the operations run on the set
-	*					provided as originalSet. The key is the timestamp for
+	* @param opLogs		TreeMap of logs for the operations run on the SkipList
+	*					provided as startList. The key is the timestamp for
 	*					when the linearization point occurred.
 	*
 	* @return			the number of erroneous operations in operationLogs
 	*/
-	public static int checkLogs(LinkedList<Integer> startSet,
+	public static int checkLogs(LinkedList<Integer> startList,
 								TreeMap<long, Log> opLogs) {
-		LockFreeSkipList skipList = convertToSkipList(originalSet)
 		
 		int erroneousOps = 0;
+		LockFreeSkipList skipList = convertLLToSkipList(startList);
 
-		for(log in operationLogs) {
-			erronousOps += operationIsErronous ? 1 : 0;
+		for(Map.Entry<long, Log> mapEntry : opLogs.entrySet()) {
+			Log opLog = mapEntry.getValue();
+			erronousOps += isOperationValid(opLog, skipList) ? 1 : 0;
 		}
 
 		return erronousOps;
+	}
+
+	/**
+	* Given an operation, the set the operation is being performed on, and
+	* the expected successfulness of the operation, return whether the
+	* operation is valid. If the successfulness of the operation found in the
+	* given operation log does not match the actual operation, then the
+	* operation is not valid.
+	*
+	* @param opLog	Log of an operation
+	* @param list	LockFree list on which the operation represented by opLog
+	*				will be performed
+	* @return whether the operation represented by opLog is valid
+	*/
+	private static boolean isOperationValid(Log opLog, LockFreeSkipList list) {
+		
+		Log result;
+
+		// In this case, the linearization point happened in a different thread
+		// so want to ignore the log
+		if (opLog.timestamp == 0) {
+			return true;
+		} else if (opLog.operation.equals("add")) {
+			result = list.add(opLog.data);
+		} else if (opLog.operation.equals("remove")) {
+			result = list.remove(opLog.data);
+		} else if (opLog.operation.equals("contains")) {
+			result = list.contains(opLog.data);
+		}
+
+		return result.successful == opLog.successful;
+	}
+
+	/**
+	* Convert a Linked List into a LockFreeSkipList.
+	*
+	* @param ll	LinkedList to convert
+	* @return LockFreeSkipList with the elements from the passed LinkedList
+	*/
+	public static int convertLLToSkipList(LinkedList<Integer> ll) {
+		LockFreeSkipList skipList = new LockFreeSkipList(false);
+
+		for (Integer elem : ll) {
+			skipList.add(elem);
+		}
+
+		return skipList;
 	}
 
 }
