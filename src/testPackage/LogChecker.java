68605ce373e1117cefe94a05596c98de8fc32d27
Kai Fleischman <kaifleischmans@gmail.com>
2020-10-04 23:17:56 +0200

    spel chekker

--- src/testPackage/LogChecker.java
+++ src/testPackage/LogChecker.java
@@ -12,13 +12,13 @@ public class LogChecker {
 	*					provided as originalSet. The key is the timestamp for
 	*					when the linearization point occurred.
 	*
-	* @return			the number of erronous operations in operationLogs
+	* @return			the number of erroneous operations in operationLogs
 	*/
 	public static int checkLogs(LinkedList<Integer> startSet,
 								TreeMap<long, Log> opLogs) {
 		LockFreeSkipList skipList = convertToSkipList(originalSet)
 		
-		int erronousOps = 0;
+		int erroneousOps = 0;
 
 		for(log in operationLogs) {
 			erronousOps += operationIsErronous ? 1 : 0;
